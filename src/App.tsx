76a233401499409c65476832f055e2f7ac8adcac
yahyaemadn2-lang <yahyaemadn2@gmail.com>
2025-12-15 01:07:00 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, useLocation } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import { CartProvider } from './contexts/CartContext';
 import Navbar from './components/Navbar';
 import AppRoutes from './routes';
+
+function AppContent() {
+  const location = useLocation();
+  const isDashboardRoute = location.pathname.startsWith('/dashboard');
+
+  return (
+    <>
+      {!isDashboardRoute && <Navbar />}
+      <AppRoutes />
+    </>
+  );
+}
 
 function App() {
   return (
     <Router>
       <AuthProvider>
         <CartProvider>
-          <Navbar />
-          <AppRoutes />
+          <AppContent />
         </CartProvider>
       </AuthProvider>
     </Router>
